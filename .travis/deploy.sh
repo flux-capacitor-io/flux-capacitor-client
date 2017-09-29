02c6436f6ae5439a366e783a2131b8f44d2b7cd7
rene <renedewaele@gmail.com>
2017-09-30 00:55:50 +0200

    Debugging deploy script issues

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,16 +6,16 @@ set -e
 if [ ! -z "$TRAVIS_TAG" ]
 then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
-    mvn --settings ./settings.xml org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion=$TRAVIS_TAG -Prelease
+    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion=$TRAVIS_TAG -Prelease
 
     if [ ! -z "$TRAVIS" -a -f "$HOME/.gnupg" ]; then
         shred -v ~/.gnupg/*
         rm -rf ~/.gnupg
     fi
 
-    source ./gpg.sh
+    source .travis/gpg.sh
 
-    mvn clean deploy --settings ./settings.xml -DskipTests=true --batch-mode --update-snapshots -Prelease
+    mvn clean deploy --settings .travis/settings.xml -DskipTests=true --batch-mode --update-snapshots -Prelease
 
 
     if [ ! -z "$TRAVIS" ]; then
