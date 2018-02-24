23efac088cda3775d84c6ed1fac7873e8eb1541b
Rene de Waele <renedewaele@gmail.com>
2018-02-24 17:25:47 +0100

    Fix issue with deserialization of ValidationException

--- java-client/src/main/java/io/fluxcapacitor/javaclient/tracking/handling/validation/ValidationException.java
+++ java-client/src/main/java/io/fluxcapacitor/javaclient/tracking/handling/validation/ValidationException.java
@@ -3,23 +3,37 @@ package io.fluxcapacitor.javaclient.tracking.handling.validation;
 import lombok.Getter;
 
 import javax.validation.ConstraintViolation;
+import java.beans.ConstructorProperties;
 import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
+import static java.util.stream.Collectors.toCollection;
 import static java.util.stream.Collectors.toList;
 
 @Getter
 public class ValidationException extends RuntimeException {
 
-    private final Set<? extends ConstraintViolation<?>> violations;
+    private final SortedSet<String> violations;
 
     public ValidationException(Set<? extends ConstraintViolation<?>> violations) {
-        super(String.format("One or more constraints were violated:%s%s", System.lineSeparator(), convert(violations)));
-        this.violations = violations;
+        super(String.format("One or more constraints were violated:%s%s", System.lineSeparator(), format(violations)));
+        this.violations = violations.stream().map(ValidationException::format).collect(toCollection(TreeSet::new));
     }
 
-    protected static String convert(Set<? extends ConstraintViolation<?>> violations) {
-        return String.join(System.lineSeparator(), violations.stream().map(v -> String
-                .format("property %s in class %s %s", v.getPropertyPath(), v.getRootBeanClass().getSimpleName(),
-                        v.getMessage())).sorted().collect(toList()));
+    @ConstructorProperties({"message", "violations"})
+    public ValidationException(String message, Set<String> violations) {
+        super(message);
+        this.violations = new TreeSet<>(violations);
+    }
+
+    protected static String format(ConstraintViolation<?> v) {
+        return String.format("property %s in class %s %s", v.getPropertyPath(), v.getRootBeanClass().getSimpleName(),
+                             v.getMessage());
+    }
+
+    protected static String format(Set<? extends ConstraintViolation<?>> violations) {
+        return String.join(System.lineSeparator(), violations.stream().map(
+                ValidationException::format).sorted().collect(toList()));
     }
 }
