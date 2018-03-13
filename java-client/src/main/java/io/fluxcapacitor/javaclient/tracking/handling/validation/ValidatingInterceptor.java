668a0d3f68a0dd0b136eac8488df003513bed3a0
rene <renedewaele@gmail.com>
2018-03-13 09:46:16 +0100

    Expose default validator from ValidatingInterceptor

--- java-client/src/main/java/io/fluxcapacitor/javaclient/tracking/handling/validation/ValidatingInterceptor.java
+++ java-client/src/main/java/io/fluxcapacitor/javaclient/tracking/handling/validation/ValidatingInterceptor.java
@@ -4,27 +4,22 @@ import io.fluxcapacitor.javaclient.common.serialization.DeserializingMessage;
 import io.fluxcapacitor.javaclient.tracking.handling.HandlerInterceptor;
 import lombok.AllArgsConstructor;
 
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
-import javax.validation.Validator;
-import java.util.Set;
 import java.util.function.Function;
 
 @AllArgsConstructor
 public class ValidatingInterceptor implements HandlerInterceptor {
+    public static final Validator defaultValidator = Jsr380Validator.createDefault();
+
     private final Validator validator;
 
     public ValidatingInterceptor() {
-        this.validator = Validation.buildDefaultValidatorFactory().getValidator();;
+        this.validator = defaultValidator;
     }
 
     @Override
     public Function<DeserializingMessage, Object> interceptHandling(Function<DeserializingMessage, Object> function) {
         return m -> {
-            Set<? extends ConstraintViolation<?>> violations = validator.validate(m.getPayload());
-            if (!violations.isEmpty()) {
-                throw new ValidationException(violations);
-            }
+            validator.validate(m.getPayload());
             return function.apply(m);
         };
     }
