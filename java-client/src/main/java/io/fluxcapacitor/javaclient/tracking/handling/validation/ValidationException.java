859eef438ba6f9dd71664399448cd883424c0265
Rene de Waele <renedewaele@gmail.com>
2018-12-06 15:29:08 +0100

    Log routing key of current message in validation exception

--- java-client/src/main/java/io/fluxcapacitor/javaclient/tracking/handling/validation/ValidationException.java
+++ java-client/src/main/java/io/fluxcapacitor/javaclient/tracking/handling/validation/ValidationException.java
@@ -1,16 +1,21 @@
 package io.fluxcapacitor.javaclient.tracking.handling.validation;
 
 import io.fluxcapacitor.javaclient.common.exception.FunctionalException;
+import io.fluxcapacitor.javaclient.common.serialization.DeserializingMessage;
+import io.fluxcapacitor.javaclient.publishing.routing.RoutingKey;
 import lombok.Getter;
 
 import javax.validation.ConstraintViolation;
 import java.beans.ConstructorProperties;
+import java.util.Optional;
 import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
+import static io.fluxcapacitor.common.reflection.ReflectionUtils.getAnnotatedPropertyValue;
+import static java.lang.System.lineSeparator;
+import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toCollection;
-import static java.util.stream.Collectors.toList;
 
 @Getter
 public class ValidationException extends FunctionalException {
@@ -18,8 +23,15 @@ public class ValidationException extends FunctionalException {
     private final SortedSet<String> violations;
 
     public ValidationException(Set<? extends ConstraintViolation<?>> violations) {
-        super(String.format("One or more constraints were violated:%s%s", System.lineSeparator(), format(violations)));
+        super(String.format("One or more constraints were violated%s:%s%s", getContext(), lineSeparator(),
+                            format(violations)));
         this.violations = violations.stream().map(ValidationException::format).collect(toCollection(TreeSet::new));
+    }
+
+    private static String getContext() {
+        return Optional.ofNullable(DeserializingMessage.getCurrent()).filter(DeserializingMessage::isDeserialized)
+                .map(m -> getAnnotatedPropertyValue(m.getPayload(), RoutingKey.class)
+                        .map(v -> String.format(" (routing key= %s)", v)).orElse("")).orElse("");
     }
 
     @ConstructorProperties({"message", "violations"})
@@ -34,7 +46,6 @@ public class ValidationException extends FunctionalException {
     }
 
     protected static String format(Set<? extends ConstraintViolation<?>> violations) {
-        return String.join(System.lineSeparator(), violations.stream().map(
-                ValidationException::format).sorted().collect(toList()));
+        return violations.stream().map(ValidationException::format).sorted().collect(joining(lineSeparator()));
     }
 }
