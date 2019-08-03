77024384f2295ca210419f1728ee9a8099800f28
Rene de Waele <renedewaele@gmail.com>
2019-08-03 16:53:23 +0200

    Try improve travis deployments to maven central

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,11 +2,5 @@
 
 set -e
 
-echo "Importing private gpg key"
-gpg --import .travis/signingkey.asc
-
-echo "Matching artifacts version to tag name"
 mvn versions:set -DnewVersion=${TRAVIS_TAG}
-
-echo "Deploying artifacts to Maven Central"
-mvn clean deploy --settings .travis/settings.xml -DskipTests=true -Dgpg.keyname=5F84CD1775351968CABF0B0D779D2423E1D24D89 -Dgpg.passphrase=$PASSPHRASE -P release
+mvn deploy --settings .travis/settings.xml -P release
