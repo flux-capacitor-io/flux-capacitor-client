7567cf3452dc1e85372e1170b9cfc7758783014a
Rene de Waele <renedewaele@gmail.com>
2019-08-03 16:01:51 +0200

    Try improve travis deployments to maven central

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,20 +2,6 @@
 
 set -e
 
-# only do deployment when travis detects a new tag
-if [ ! -z "$TRAVIS_TAG" ]
-then
-    echo "commit is tagged with $TRAVIS_TAG"
-
-    # only deploy tags if they start with a number
-    if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ $TRAVIS_TAG =~ ^[0-9].*$ ];
-    then
-        echo "deploying release $TRAVIS_TAG"
-        mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion=$TRAVIS_TAG -Prelease
-        mvn clean deploy --settings .travis/settings.xml -DskipTests=true -Prelease
-    else
-        echo "not deploying because the tag did not begin with a number"
-    fi
-else
-    echo "commit isn't tagged -> no need to deploy a new release"
-fi
+gpg --import .travis/signingkey.asc
+mvn versions:set -DnewVersion=${TRAVIS_TAG}
+mvn clean deploy --settings .travis/settings.xml -DskipTests=true -P release
