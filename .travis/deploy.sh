7258cf7bcaf8c3b0ffc55ee71b1503f93cc40cc3
Rene de Waele <renedewaele@gmail.com>
2019-08-03 15:45:15 +0200

    Try improve travis deployments to maven central

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -8,15 +8,11 @@ then
     echo "commit is tagged with $TRAVIS_TAG"
 
     # only deploy tags if they start with a number
-    if [[ $TRAVIS_TAG =~ ^[0-9].*$ ]];
+    if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ $TRAVIS_TAG =~ ^[0-9].*$ ];
     then
         echo "deploying release $TRAVIS_TAG"
         mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion=$TRAVIS_TAG -Prelease
-
-        gpg2 --keyring=$TRAVIS_BUILD_DIR/pubring.gpg --no-default-keyring --import .travis/signingkey.asc
-        gpg2 --allow-secret-key-import --keyring=$TRAVIS_BUILD_DIR/secring.gpg --no-default-keyring --import .travis/signingkey.asc
-
-        mvn clean deploy --settings .travis/settings.xml -Dgpg.executable=gpg2 -Dgpg.keyname=851216B95C455EF2A5FDC634905DD694F5A57F2C -Dgpg.passphrase=$PASSPHRASE -Dgpg.publicKeyring=$TRAVIS_BUILD_DIR/pubring.gpg -Dgpg.secretKeyring=$TRAVIS_BUILD_DIR/secring.gpg -DskipTests=true --batch-mode --update-snapshots -Prelease;
+        mvn clean deploy --settings .travis/settings.xml -DskipTests=true -Prelease
     else
         echo "not deploying because the tag did not begin with a number"
     fi
