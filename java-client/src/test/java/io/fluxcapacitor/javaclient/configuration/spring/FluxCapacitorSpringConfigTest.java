71c9479b5989fc677fc3f8f4d5942d2190c744da
Rene de Waele <renedewaele@gmail.com>
2020-03-12 11:35:16 +0100

    Add ability to provide the UserProvider as Spring bean

--- java-client/src/test/java/io/fluxcapacitor/javaclient/configuration/spring/FluxCapacitorSpringConfigTest.java
+++ java-client/src/test/java/io/fluxcapacitor/javaclient/configuration/spring/FluxCapacitorSpringConfigTest.java
@@ -15,13 +15,19 @@
 package io.fluxcapacitor.javaclient.configuration.spring;
 
 import com.fasterxml.jackson.databind.node.TextNode;
+import io.fluxcapacitor.common.api.Metadata;
 import io.fluxcapacitor.javaclient.FluxCapacitor;
 import io.fluxcapacitor.javaclient.common.serialization.upcasting.Upcast;
 import io.fluxcapacitor.javaclient.tracking.handling.HandleCommand;
+import io.fluxcapacitor.javaclient.tracking.handling.HandleQuery;
+import io.fluxcapacitor.javaclient.tracking.handling.authentication.User;
+import io.fluxcapacitor.javaclient.tracking.handling.authentication.UserProvider;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -30,12 +36,21 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = FluxCapacitorSpringConfigTest.Config.class)
 @Slf4j
 public class FluxCapacitorSpringConfigTest {
+    private static final User mockUser = mock(User.class);
+    private static final UserProvider mockUserProvider = mock(UserProvider.class);
+    
+    static {
+        when(mockUserProvider.fromMetadata(any(Metadata.class))).thenReturn(mockUser);
+    }
 
     @Autowired
     private FluxCapacitor fluxCapacitor;
@@ -45,12 +60,25 @@ public class FluxCapacitorSpringConfigTest {
         String result = fluxCapacitor.commandGateway().sendAndWait("command");
         assertEquals("upcasted result", result);
     }
+    
+    @Test
+    void testUserProviderInjected() {
+        assertEquals(mockUser, fluxCapacitor.queryGateway().sendAndWait(new GetUser()));
+    }
 
     @Component
     public static class SomeHandler {
         @HandleCommand
         public Object handleCommand(String command) {
             return "result";
+        }
+    }
+    
+    @Component @LocalHandler
+    public static class SomeLocalHandler {
+        @HandleQuery
+        public User handle(GetUser query) {
+            return User.getCurrent();
         }
     }
 
@@ -67,5 +95,14 @@ public class FluxCapacitorSpringConfigTest {
     @ComponentScan
     public static class Config {
 
+        @Bean
+        public UserProvider userProvider() {
+            return mockUserProvider;
+        }
+        
+    }
+    
+    @Value
+    static class GetUser {
     }
 }
