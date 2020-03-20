58614497a1a82dda7894d23432cd6a7c8767a263
Rene de Waele <renedewaele@gmail.com>
2020-03-20 14:35:48 +0100

    Remove travis configuration

--- .travis/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-gpg --allow-secret-key-import --import .travis/codesigning.asc
-mvn versions:set -DnewVersion=${TRAVIS_TAG}
-mvn deploy --settings .travis/settings.xml -DskipTests=true -P deploy
