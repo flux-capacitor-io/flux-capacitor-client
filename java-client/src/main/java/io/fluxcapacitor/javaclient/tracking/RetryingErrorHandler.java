552557017c27ef9d7317a152ee5adb5359cc3f0d
Rene de Waele <renedewaele@gmail.com>
2020-04-16 12:24:03 +0200

    Have RetryingErrorHandler log functional errors on warn level

--- java-client/src/main/java/io/fluxcapacitor/javaclient/tracking/RetryingErrorHandler.java
+++ java-client/src/main/java/io/fluxcapacitor/javaclient/tracking/RetryingErrorHandler.java
@@ -1,5 +1,6 @@
 package io.fluxcapacitor.javaclient.tracking;
 
+import io.fluxcapacitor.javaclient.common.exception.FunctionalException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -8,6 +9,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Predicate;
 
 import static io.fluxcapacitor.common.TimingUtils.retryOnFailure;
+import static java.lang.String.format;
 
 @Slf4j
 @AllArgsConstructor
@@ -16,25 +18,27 @@ public class RetryingErrorHandler implements ErrorHandler {
     private final Duration delay;
     private final Predicate<Exception> errorFilter;
     private final boolean throwOnFailure;
+    private final boolean logFunctionalErrors;
 
     public static RetryingErrorHandler forAnyError() {
         return new RetryingErrorHandler(e -> true);
     }
 
     public RetryingErrorHandler(Predicate<Exception> errorFilter) {
-        this(5, Duration.ofSeconds(2), errorFilter, false);
+        this(5, Duration.ofSeconds(2), errorFilter, false, true);
     }
 
     @Override
     public void handleError(Exception error, String errorMessage, Runnable retryFunction) throws Exception {
         if (!errorFilter.test(error)) {
-            log.error("{}. Not retrying, {}", errorMessage, throwOnFailure ? "propagating error" : "continuing.", error);
+            logError(format("%s. Not retrying, %s", errorMessage, throwOnFailure ? "propagating error" : "continuing."),
+                     error);
             if (throwOnFailure) {
                 throw error;
             }
             return;
         }
-        log.error("{}. Retrying up to {} times.", errorMessage, retries, error);
+        logError(format("%s. Retrying up to %s times.", errorMessage, retries), error);
         AtomicInteger remainingRetries = new AtomicInteger(retries);
         boolean success = retryOnFailure(retryFunction, delay,
                                          e -> errorFilter.test(e) && remainingRetries.decrementAndGet() > 0);
@@ -42,11 +46,32 @@ public class RetryingErrorHandler implements ErrorHandler {
             log.info("Message handling was successful on retry");
         } else {
             if (throwOnFailure) {
-                log.error("Propagating error");
+                logMessage("Propagating error", isTechnicalError(error));
                 throw error;
             } else {
-                log.error("{}. Not retrying any further. Continuing with next handler.", errorMessage);
+                logMessage(format("%s. Not retrying any further. Continuing with next handler.", errorMessage),
+                           isTechnicalError(error));
             }
         }
+    }
+
+    protected void logError(String errorMessage, Exception error) {
+        if (isTechnicalError(error)) {
+            log.error("{}. Continuing...", errorMessage, error);
+        } else if (logFunctionalErrors) {
+            log.warn("{}. Continuing...", errorMessage, error);
+        }
+    }
+
+    protected void logMessage(String errorMessage, boolean severe) {
+        if (severe) {
+            log.error(errorMessage);
+        } else {
+            log.warn(errorMessage);
+        }
+    }
+
+    protected boolean isTechnicalError(Exception error) {
+        return !(error instanceof FunctionalException);
     }
 }
