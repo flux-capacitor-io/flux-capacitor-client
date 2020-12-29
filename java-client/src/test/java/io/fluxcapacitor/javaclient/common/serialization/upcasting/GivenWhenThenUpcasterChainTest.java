539c41f2af788e3729397ad656dbbe9aea51d408
Rene de Waele <renedewaele@gmail.com>
2020-12-29 16:23:27 +0100

    Have givenDomainEvents accept Data objects as well

--- java-client/src/test/java/io/fluxcapacitor/javaclient/common/serialization/upcasting/GivenWhenThenUpcasterChainTest.java
+++ java-client/src/test/java/io/fluxcapacitor/javaclient/common/serialization/upcasting/GivenWhenThenUpcasterChainTest.java
@@ -1,5 +1,20 @@
+/*
+ * Copyright (c) 2016-2020 Flux Capacitor.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.fluxcapacitor.javaclient.common.serialization.upcasting;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.fluxcapacitor.common.api.Data;
@@ -124,6 +139,7 @@ public class GivenWhenThenUpcasterChainTest {
         @EventSourced
         @Value
         public static class TestModel {
+            @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS)
             List<Object> events;
 
             @ApplyEvent
