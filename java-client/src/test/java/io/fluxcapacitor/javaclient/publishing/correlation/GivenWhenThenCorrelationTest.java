8bc65623d948ee75e9363786aa089635ed976158
Rene de Waele <renedewaele@gmail.com>
2021-05-26 08:25:34 +0200

    Fix build

--- java-client/src/test/java/io/fluxcapacitor/javaclient/publishing/correlation/GivenWhenThenCorrelationTest.java
+++ java-client/src/test/java/io/fluxcapacitor/javaclient/publishing/correlation/GivenWhenThenCorrelationTest.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright (c) 2016-2021 Flux Capacitor.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.fluxcapacitor.javaclient.publishing.correlation;
 
 import io.fluxcapacitor.common.api.Metadata;
@@ -15,6 +29,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
+import static java.lang.Long.parseLong;
 import static java.util.Collections.singletonList;
 import static java.util.Optional.ofNullable;
 
@@ -29,8 +44,7 @@ public class GivenWhenThenCorrelationTest {
                 .expectResult((Predicate<TestModel>) r -> r.getEventMetadata().size() == 1 &&
                         ofNullable(r.getEventMetadata().get(0))
                                 .map(m -> m.get("$consumer").equals(m.get("$clientName") + "_COMMAND")
-                                        && m.get("$traceId").equals("0")
-                                        && m.get("$correlationId").equals("0")
+                                        && parseLong(m.get("$correlationId")) == parseLong(m.get("$traceId"))
                                         && m.get("$trigger").equals(CreateModel.class.getName())
                                 ).orElse(false));
     }
@@ -42,8 +56,7 @@ public class GivenWhenThenCorrelationTest {
                 .expectResult((Predicate<TestModel>) r -> r.getEventMetadata().size() == 1 &&
                         ofNullable(r.getEventMetadata().get(0))
                                 .map(m -> m.get("$consumer").equals(m.get("$clientName") + "_COMMAND")
-                                        && m.get("$traceId").equals("0")
-                                        && m.get("$correlationId").equals("1")
+                                        && parseLong(m.get("$correlationId")) > parseLong(m.get("$traceId"))
                                         && m.get("$trigger").equals(CreateModel.class.getName())
                                 ).orElse(false));
     }
--- java-client/src/test/java/io/fluxcapacitor/javaclient/tracking/ThrowingErrorHandlerTest.java
+++ java-client/src/test/java/io/fluxcapacitor/javaclient/tracking/ThrowingErrorHandlerTest.java
@@ -15,10 +15,12 @@
 package io.fluxcapacitor.javaclient.tracking;
 
 import io.fluxcapacitor.common.MessageType;
+import io.fluxcapacitor.javaclient.common.serialization.DeserializingMessage;
 import io.fluxcapacitor.javaclient.configuration.DefaultFluxCapacitor;
 import io.fluxcapacitor.javaclient.test.TestFixture;
 import io.fluxcapacitor.javaclient.tracking.client.TrackingClient;
 import io.fluxcapacitor.javaclient.tracking.handling.HandleEvent;
+import lombok.Getter;
 import org.junit.jupiter.api.Test;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -29,10 +31,10 @@ import static org.mockito.Mockito.verify;
 
 class ThrowingErrorHandlerTest {
 
+    private final ThrowingEventHandler eventHandler = new ThrowingEventHandler();
     private final TestFixture testFixture = TestFixture.createAsync(
             DefaultFluxCapacitor.builder().configureDefaultConsumer(MessageType.EVENT, c -> c.toBuilder()
-                    .errorHandler(new ThrowingErrorHandler()).build()),
-            new ThrowingEventHandler());
+                    .errorHandler(new ThrowingErrorHandler()).build()), eventHandler);
 
     @Test
     void testTrackingStoppedDuringFirstEvent() {
@@ -46,18 +48,23 @@ class ThrowingErrorHandlerTest {
         testFixture.whenDomainEvents("testAggregate", 1, "error")
                 .expectThat(fc -> {
                     TrackingClient trackingClient = fc.client().getTrackingClient(MessageType.EVENT);
-                    verify(trackingClient).storePosition(any(), any(), eq(0L));
-                    verify(trackingClient, never()).storePosition(any(), any(), eq(1L));
+                    verify(trackingClient).storePosition(any(), any(), eq((long) eventHandler.getFirstIndex()));
+                    verify(trackingClient, never()).storePosition(any(), any(), eq((long) eventHandler.getSecondIndex()));
                 });
     }
 
+    @Getter
     private static class ThrowingEventHandler {
+        private volatile Long firstIndex, secondIndex;
+
         @HandleEvent
-        private void handle(Integer event) {
+        private void handle(Integer event, DeserializingMessage message) {
+            firstIndex = message.getSerializedObject().getIndex();
         }
 
         @HandleEvent
-        private void handle(String event) {
+        private void handle(String event, DeserializingMessage message) {
+            secondIndex = message.getSerializedObject().getIndex();
             throw new IllegalArgumentException();
         }
     }
