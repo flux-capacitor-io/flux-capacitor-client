b0ad92c7a7079524a24f7b388479a9f8d9da7ecc
Rene de Waele <rene@flux-capacitor.io>
2022-11-02 07:16:30 +0100

    Ensure that a serialized document never has an end timestamp before the start timestamp
    This may prevent it from being found.

--- common/src/main/java/io/fluxcapacitor/common/api/search/SerializedDocument.java
+++ common/src/main/java/io/fluxcapacitor/common/api/search/SerializedDocument.java
@@ -47,6 +47,10 @@ public class SerializedDocument {
         summary = document.summarize();
     }
 
+    public Long getEnd() {
+        return end == null || timestamp == null || end > timestamp ? end : timestamp;
+    }
+
     @Getter(lazy = true)
     @Accessors(fluent = true)
     @EqualsAndHashCode.Exclude
