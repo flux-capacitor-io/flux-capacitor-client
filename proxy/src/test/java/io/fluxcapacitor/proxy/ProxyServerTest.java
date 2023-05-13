6cf64883682b8fe87092d5823183809cba1be67d
Rene de Waele <rene@flux-capacitor.io>
2023-05-13 20:40:21 +0200

    Try get tests to work in github actions

--- proxy/src/test/java/io/fluxcapacitor/proxy/ProxyServerTest.java
+++ proxy/src/test/java/io/fluxcapacitor/proxy/ProxyServerTest.java
@@ -1,6 +1,7 @@
 package io.fluxcapacitor.proxy;
 
 import io.fluxcapacitor.common.Registration;
+import io.fluxcapacitor.common.TestUtils;
 import io.fluxcapacitor.common.ThrowingConsumer;
 import io.fluxcapacitor.common.ThrowingFunction;
 import io.fluxcapacitor.javaclient.FluxCapacitor;
@@ -33,7 +34,7 @@ import static java.lang.String.format;
 class ProxyServerTest {
 
     private final TestFixture testFixture = TestFixture.createAsync();
-    private final int proxyPort = 8080;
+    private final int proxyPort = TestUtils.getAvailablePort();
     private final ProxyRequestHandler proxyRequestHandler =
             new ProxyRequestHandler(testFixture.getFluxCapacitor().client());
     private final Registration proxyServer = ProxyServer.start(proxyPort, proxyRequestHandler);
