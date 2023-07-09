2a2308d5a9fe4bf526ce7cd5239a998e142c062f
Rene de Waele <rene@flux-capacitor.io>
2023-07-09 08:29:27 +0200

    Make ProxyServer testable outside of flux client

--- proxy/src/main/java/io/fluxcapacitor/proxy/ProxyServer.java
+++ proxy/src/main/java/io/fluxcapacitor/proxy/ProxyServer.java
@@ -15,18 +15,14 @@
 package io.fluxcapacitor.proxy;
 
 import io.fluxcapacitor.common.Registration;
-import io.fluxcapacitor.javaclient.FluxCapacitor;
 import io.fluxcapacitor.javaclient.configuration.client.Client;
 import io.fluxcapacitor.javaclient.configuration.client.WebSocketClient;
 import io.undertow.Undertow;
-import io.undertow.server.HttpHandler;
-import io.undertow.server.handlers.GracefulShutdownHandler;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Optional;
 
 import static io.undertow.Handlers.path;
-import static java.lang.Runtime.getRuntime;
 
 @Slf4j
 public class ProxyServer {
@@ -39,7 +35,7 @@ public class ProxyServer {
         start(port, new ProxyRequestHandler(client));
     }
 
-    static Registration start(int port, ProxyRequestHandler proxyHandler) {
+    public static Registration start(int port, ProxyRequestHandler proxyHandler) {
         Undertow server = Undertow.builder().addHttpListener(port, "0.0.0.0")
                 .setHandler(path().addPrefixPath("/", proxyHandler))
                 .build();
@@ -49,22 +45,5 @@ public class ProxyServer {
             proxyHandler.close();
             server.stop();
         };
-    }
-
-    private static GracefulShutdownHandler addShutdownHandler(HttpHandler httpHandler, Runnable proxyShutdownHandler) {
-        GracefulShutdownHandler shutdownHandler = new GracefulShutdownHandler(httpHandler);
-        Runnable task = () -> {
-            proxyShutdownHandler.run();
-            shutdownHandler.shutdown();
-            try {
-                shutdownHandler.awaitShutdown(1000);
-            } catch (InterruptedException e) {
-                log.warn("Thread to kill server was interrupted");
-                Thread.currentThread().interrupt();
-            }
-        };
-        FluxCapacitor.getOptionally().ifPresentOrElse(fc -> fc.beforeShutdown(task),
-                                                      () -> getRuntime().addShutdownHook(new Thread(task)));
-        return shutdownHandler;
     }
 }
