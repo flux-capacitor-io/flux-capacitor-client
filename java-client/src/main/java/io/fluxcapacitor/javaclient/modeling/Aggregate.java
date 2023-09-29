200c60024c8daa850f4224d18e78d7263dc2d04f
Rene de Waele <rene@flux-capacitor.io>
2023-09-29 21:00:01 +0200

    Fix build

--- java-client/src/main/java/io/fluxcapacitor/javaclient/modeling/Aggregate.java
+++ java-client/src/main/java/io/fluxcapacitor/javaclient/modeling/Aggregate.java
@@ -45,9 +45,9 @@ public @interface Aggregate {
      * of the event. I.e. even if your code does not invoke {@link Entity#previous()} anywhere, this setting will still
      * be relevant.
      * <p>
-     * A depth < 0 implies that every version of the aggregate will be cached. For event sourced aggregates that can
-     * become quite large (n >> 100) it is therefore advisable to select a depth >= 0. Note that a depth of 0 only
-     * caches the most recent version of the aggregate.
+     * A negative depth (the default) implies that every version of the aggregate will be cached. For event sourced
+     * aggregates that can become quite large (events in the 100s or more) it is therefore advisable to specify a depth.
+     * Note that a depth of 0 only caches the most recent version of the aggregate.
      * <p>
      * This setting does nothing if {@link #cached()} or {@link #eventSourced()} is {@code false}.
      */
@@ -57,11 +57,11 @@ public @interface Aggregate {
      * Setting to control after which number of applied events the state of an aggregate should be stored in the cache
      * to serve as checkpoint for event sourcing when requesting later version of the aggregate.
      * <p>
-     * Note that this setting only applies for older versions of the aggregate, i.e. for sequence numbers <= than the
-     * most recent sequence number minus the {@link #cachingDepth()}.
+     * Note that this setting only applies for older versions of the aggregate, i.e. for sequence numbers smaller than
+     * or equal to the most recent sequence number minus the {@link #cachingDepth()}.
      * <p>
-     * This period should be >= 1. For a period of 1 all versions of the aggregate will be cached, which can also be
-     * achieved by selecting a {@link #cachingDepth()} < 0 (the default).
+     * This period should be greater than or equal to 1. For a period of 1 all versions of the aggregate will be cached,
+     * which can also be achieved by selecting a negative {@link #cachingDepth()} (the default).
      * <p>
      * This setting does nothing if {@link #cached()} or {@link #eventSourced()} is {@code false}.
      */
