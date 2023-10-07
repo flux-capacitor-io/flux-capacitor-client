c55f8dbbe5137cb63e8bc955be75ff117035b1c6
Rene de Waele <rene@flux-capacitor.io>
2023-10-07 15:13:44 +0200

    Fix javadoc

--- common/src/main/java/io/fluxcapacitor/common/search/SearchInclude.java
+++ common/src/main/java/io/fluxcapacitor/common/search/SearchInclude.java
@@ -23,13 +23,13 @@ import java.lang.annotation.Target;
 /**
  * Marker annotation that indicates that a property is to be included when indexing a document for search. When a
  * property is included, the document can be matched using this property. Note that inclusion is the default for
- * properties, so this annotation is only useful to override the effects of {@link SearchExclude(true)} on a parent or
+ * properties, so this annotation is only useful to override the effects of {@link SearchExclude}(true) on a parent or
  * ancestor.
  * <p>
  * When this annotation is present on a type, all properties of the class will be included when indexing, unless they
- * are individually annotated with {@link SearchExclude(true)}.
+ * are individually annotated with {@link SearchExclude}(true).
  * <p>
- * Note that this annotation is an alias for {@link SearchExclude(false)} and has the same effect.
+ * Note that this annotation is an alias for {@link SearchExclude}(false) and has the same effect.
  */
 @Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD, ElementType.FIELD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
