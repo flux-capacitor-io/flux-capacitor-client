679063f46b14fe21dd63cbea95122450f70078bb
Rene de Waele <rene@flux-capacitor.io>
2024-05-13 16:10:34 +0200

    Strip strings by default during deserialization

--- common/src/main/java/io/fluxcapacitor/common/serialization/StripStringsModule.java
+++ common/src/main/java/io/fluxcapacitor/common/serialization/StripStringsModule.java
@@ -38,7 +38,7 @@ public class StripStringsModule extends SimpleModule {
         @SneakyThrows
         public String deserialize(JsonParser parser, DeserializationContext context) {
             String result = parser.getValueAsString();
-            return result.isBlank() ? null : result;
+            return result.isBlank() ? null : result.trim();
         }
     }
 }
