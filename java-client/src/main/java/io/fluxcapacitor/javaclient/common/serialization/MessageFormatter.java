e0df1c841ebb6fe8e108fd50e2d644fe648f6edb
Rene de Waele <rene@flux-capacitor.io>
2024-06-07 17:53:10 +0200

    Remove routing key in default MessageFormatter

--- java-client/src/main/java/io/fluxcapacitor/javaclient/common/serialization/MessageFormatter.java
+++ java-client/src/main/java/io/fluxcapacitor/javaclient/common/serialization/MessageFormatter.java
@@ -14,22 +14,9 @@
 
 package io.fluxcapacitor.javaclient.common.serialization;
 
-import io.fluxcapacitor.javaclient.publishing.routing.RoutingKey;
-
 import java.util.function.Function;
-
-import static io.fluxcapacitor.common.reflection.ReflectionUtils.getAnnotatedPropertyValue;
-import static java.lang.String.format;
 
 @FunctionalInterface
 public interface MessageFormatter extends Function<DeserializingMessage, String> {
-    MessageFormatter DEFAULT = m -> m.isDeserialized() ? getAnnotatedPropertyValue(m.getPayload(), RoutingKey.class)
-            .map(key -> format("%s (routing key: %s)", m.getPayloadClass().getSimpleName(), key))
-            .orElseGet(() -> {
-                try {
-                    return m.getPayloadClass().getSimpleName();
-                } catch (Exception e) {
-                    return m.getType();
-                }
-            }) : m.getType();
+    MessageFormatter DEFAULT = m -> m.isDeserialized() ? m.getPayloadClass().getSimpleName() : m.getType();
 }
