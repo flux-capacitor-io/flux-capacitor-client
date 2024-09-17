bdd53c36e1f5753f86eef35bfb629bc342289eba
Rene de Waele <rene@flux-capacitor.io>
2024-09-17 15:44:27 +0200

    Add ability to retain headers in web responses

--- java-client/src/main/java/io/fluxcapacitor/javaclient/web/WebResponse.java
+++ java-client/src/main/java/io/fluxcapacitor/javaclient/web/WebResponse.java
@@ -39,8 +39,10 @@ import java.beans.ConstructorProperties;
 import java.net.HttpCookie;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -171,6 +173,12 @@ public class WebResponse extends Message {
 
     public List<String> getHeaders(String name) {
         return headers.getOrDefault(name, Collections.emptyList());
+    }
+
+    public WebResponse retainHeaders(String... headerNames) {
+        var filtered = new LinkedHashMap<>(headers);
+        filtered.keySet().retainAll(Arrays.stream(headerNames).map(WebUtils::fixHeaderName).toList());
+        return toBuilder().headers(filtered).build();
     }
 
     public String getHeader(String name) {
